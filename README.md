f9b8ace0e1b9479897acc17eef5dbaabf9994ee1
Marco Martinez <m-martinez@users.noreply.github.com>
2017-11-10 15:39:59 -0800

    Initial import

---
+++ README.md
@@ -0,0 +1,68 @@
+# pdfr
+
+A tool that generates a PDF from an re-usable HTML template
+
+
+## Background
+
+Several project require PDF report generation with included images and graphs,
+such as application validation/verification via automaticed testing,
+templated reports, customized documents, etc. Therefore the need for a serivce
+to automate this workflow is desired.
+
+The component will take a template, render with parametrs, apply a set of style
+sheets, include images, and output a PDF file.
+
+
+## Project Goals
+
+* Must include dynamic templates (user should be able to choose template)
+* Should be able to update template without having to update software
+* Must be able to specify a header and footer
+* Must be able to include images
+* Must be able to include CSS styling
+
+
+## Implementation
+
+### Handlebars.js
+
+This tool uses [handlebars.js](http://handlebarsjs.com/) for template rendering.
+For documentation about allowed constructs see:
+
+* Expressions: http://handlebarsjs.com/expressions.html
+* Blocks: http://handlebarsjs.com/block_helpers.html
+
+### html-pdf
+
+This tool uses [html-pdf](https://github.com/marcbachmann/node-html-pdf) for
+generating a PDF from html content.
+For more documentation about configuration options, see:
+
+https://github.com/marcbachmann/node-html-pdf#options
+
+
+## Requirements
+
+* node (carbon)
+
+
+## Installation
+
+```
+  > npm install pdfr
+```
+
+
+## Development
+
+```
+  > git clone https://github.com/Codebiosys/pdfr.git
+  > cd pdfr
+  > npm install yarn && yarn
+  > yarn link
+```
+
+Remember to run `yarn link` so that the command line script is properly
+installed for testing during development
+
---
+++ bin/index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env babel-node --
+
+/**
+ * Command-line interface to PDFr functions.
+ *
+ * The intention of this script is to be callable from some deamon services
+ * such as a dedicated Docker microservice
+ */
+
+import path from 'path';
+
+import minimist from 'minimist';
+
+import { templateToPDF } from '../lib/';
+
+/**
+ * Help message printer
+ *
+ * @param {string[]} argv - command line arguments, usually from 'process.argv'
+ */
+function help(argv) {
+  const appName = path.basename(argv[1]);
+  const message = [
+    'Generates a PDF from an input HTML template with optional parameters.',
+    '',
+    `Usage: ${appName} [-c <pdf-settings>] [-p <template-variables>] <source> <destination>`,
+    '',
+    'Examples:',
+    `${appName} /path/to/source.tpl.html /path/to/destination.pdf`,
+    `${appName} -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
+    `${appName} -c settings.json  -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
+  ].join('\n');
+
+  console.log(message);
+  process.exit();
+}
+
+/**
+ * Main driver function. Prints help() if no paramters or help specified.
+ *
+ * @param {string[]} argv - command line arguments, usually from 'process.argv'
+ */
+function main(argv) {
+  const userParameters = minimist(argv.slice(2));
+
+  if (!userParameters || userParameters.h || userParameters.help) {
+    help(argv);
+  }
+
+  const {
+    _: [sourcePath, destinationPath],
+    c: pdfSettingsPath = null,
+    p: contextPath = null,
+    v: debug,
+  } = userParameters;
+
+  if (debug) {
+    console.debug = (...args) => console.log.apply(null, args);
+  }
+
+  templateToPDF(sourcePath, destinationPath, contextPath, pdfSettingsPath);
+}
+
+main(process.argv);
