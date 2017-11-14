90ae277f52257a87294d9d8d6a5a1cdd37950561
Marco Martinez <m-martinez@users.noreply.github.com>
2017-11-14 13:26:16 -0800

    100% code coveage

--- bin/cli.js
+++ bin/cli.js
@@ -11,29 +11,7 @@ import path from 'path';
 
 import minimist from 'minimist';
 
-import { templateToPDF } from '../lib/';
-
-/**
- * Help message printer
- *
- * @param {string[]} argv - command line arguments, usually from 'process.argv'
- */
-export function help(argv) {
-  const appName = path.basename(argv[1]);
-  const message = [
-    'Generates a PDF from an input HTML template with optional parameters.',
-    '',
-    `Usage: ${appName} [-c <pdf-settings>] [-p <template-variables>] <source> <destination>`,
-    '',
-    'Examples:',
-    `${appName} /path/to/source.tpl.html /path/to/destination.pdf`,
-    `${appName} -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
-    `${appName} -c settings.json  -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
-  ].join('\n');
-
-  console.log(message);
-  process.exit();
-}
+import { log, templateToPDF } from '../lib/';
 
 /**
  * Main driver function. Prints help() if no paramters or help specified.
@@ -44,7 +22,21 @@ export default function main(argv) {
   const userParameters = minimist(argv.slice(2));
 
   if (!userParameters || userParameters.h || userParameters.help) {
-    help(argv);
+    const appName = path.basename(argv[1]);
+    const message = [
+      'Generates a PDF from an input HTML template with optional parameters.',
+      '',
+      `Usage: ${appName} [-c <pdf-settings>] [-p <template-variables>] <source> <destination>`,
+      '',
+      'Examples:',
+      `${appName} /path/to/source.tpl.html /path/to/destination.pdf`,
+      `${appName} -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
+      `${appName} -c settings.json  -p context.json /path/to/source.tpl.html /path/to/destination.pdf`,
+    ].join('\n');
+
+    log.info(message);
+    process.exitCode = 1;
+    return;
   }
 
   const {
@@ -55,12 +47,14 @@ export default function main(argv) {
   } = userParameters;
 
   if (verbose) {
-    console.debug = (...args) => console.log.apply(null, args);
+    log.level = 'verbose';
   }
 
   templateToPDF(sourcePath, destinationPath, contextPath, pdfSettingsPath);
 }
 
+// Ignore CLI entry-point as this will only work when invoked directly as script
+/* istanbul ignore next */
 if (require.main === module) {
   main(process.argv);
 }
--- bin/cli.spec.js
+++ bin/cli.spec.js
@@ -1,10 +1,37 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+jest.mock('../lib');
 
-import * as sourceModule from './cli';
+import main from './cli';
+import * as lib from '../lib';
 
-beforeEach(() => {
-  console.debug = () => {};
+test('Help is called when specified via -h', () => {
+  main(['node', '/path/to/pdfr', '-h']);
+  expect(lib.log.info).toHaveBeenCalledWith(expect.stringMatching(/usage/i));
 });
 
+test('Log level can be configured via -v', () => {
+  main(['node', '/path/to/pdfr', '-v']);
+  expect(lib.log.level).toBe('verbose');
+});
+
+test('API method is called at least with a source and destination path', () => {
+  const source = '/path/to/source.html';
+  const destination = '/path/to/destination.pdf';
+  main(['node', '/path/to/pdfr', source, destination]);
+  expect(lib.templateToPDF).toHaveBeenCalledWith(source, destination, null, null);
+});
+
+test('API can be passed PDF settings', () => {
+  const source = '/path/to/source.html';
+  const destination = '/path/to/destination.pdf';
+  const settings = '/path/to/settings.json';
+  main(['node', '/path/to/pdfr', '-c', settings, source, destination]);
+  expect(lib.templateToPDF).toHaveBeenCalledWith(source, destination, null, settings);
+});
+
+test('API can be passed JSON template parameters', () => {
+  const source = '/path/to/source.html';
+  const destination = '/path/to/destination.pdf';
+  const view = '/path/to/view.json';
+  main(['node', '/path/to/pdfr', '-p', view, source, destination]);
+  expect(lib.templateToPDF).toHaveBeenCalledWith(source, destination, view, null);
+});
